ff3518f68fe3d3af2291f7627ec9ea8e5a7cf739
brody.sanford@gmail.com <brodysmac@Brodens-MBP.fios-router.home>
2022-05-16 20:30:45 -0400

    commit tree c880bb0216c14e579ddc42e12f4fa356a27f1657

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun (stuff = 'roller-skate') {
+    return `This Saturday, I want to ${stuff}!`
+}
+
+function mondayWork (work = 'go to the office') {
+    return `This Monday, I will ${work}.`
+}
+
+function wrapAdjective (flair = "*") {
+    return function (thing = 'special') {
+        return `You are ${flair}${thing}${flair}!`
+    }
+}
